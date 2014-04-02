0342e1d4595f716a562ab44a7051120efab33270
Mathieu Gosbee <mail@matbee.com>
2014-04-02 12:20:10 -0400

    Chromecast JS update

--- Chromecast.js
+++ Chromecast.js
@@ -2,15 +2,39 @@ var Chromecast = function () {
 	EventEmitter.call(this);
 	if (!Chromecast.instance) {
 		Chromecast.instance = this;
+		this.getDevices();
 		return this;
 	}
 	return Chromecast.instance;
 }
 Chromecast.prototype = Object.create(EventEmitter.prototype);
+Chromecast.prototype.initialize = function() {
+	this.on("device", function (id, deviceName) {
+		// 
+	});
+	this.on("deviceRemoved", function (id, deviceName) {
+		// 
+	});
+	this.on("launch", function () {
+		// 
+	});
+	this.on("launchFailed", function () {
+		// 
+	});
+	this.on("volumeChanged", function () {
+		// 
+	});
+	this.on("applicationStatusChanged", function () {
+		// 
+	});
+};
 Chromecast.prototype.echo = function(str) {
 	this.exec("echo", str, function (str) {
 		alert(str);
 	})
+};
+Chromecast.prototype.getDevices = function() {
+	this.exec("getDevices");
 };
 Chromecast.prototype.exec = function(action) {
 	var args = [].slice.call(arguments);
