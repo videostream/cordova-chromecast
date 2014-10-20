d8f0ae9829db6231150ab06d2c75e17adf7143af
Mathieu Gosbee <mail@matbee.com>
2014-10-20 13:10:27 -0400

    Revert "Implemented support for metadata GENERIC type"

--- chrome.cast.js
+++ chrome.cast.js
@@ -703,7 +703,8 @@ chrome.cast.Session.prototype.loadMedia = function (loadRequest, successCallback
 	var self = this;
 
 	var mediaInfo = loadRequest.media;
-	execute('loadMedia', mediaInfo.contentId, mediaInfo.contentType, mediaInfo.duration || 0.0, mediaInfo.streamType, loadRequest.autoplay || false, loadRequest.currentTime || 0, mediaInfo.metadata, function(err, obj) {
+	execute('loadMedia', mediaInfo.contentId, mediaInfo.contentType, mediaInfo.duration || 0.0, mediaInfo.streamType, loadRequest.autoplay || false, loadRequest.currentTime || 0, function(err, obj) {
+		if (!err) {
 			_currentMedia = new chrome.cast.media.Media(self.sessionId, obj.mediaSessionId);
 			_currentMedia.media = mediaInfo;
 
